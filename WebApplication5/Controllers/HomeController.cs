16fd783d3cd861be94fd7434a95d5c0fe351fc84
Akimchenko Nikita <m.r.akymchenko@student.khai.edu>
2021-06-10 12:18:47 +0300

    Added project

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            // Нужно отправить данные нового гостя no электронной почте 
+            // организатору вечеринки.
+            if (ModelState.IsValid)
+                return View("Thanks", guest);
+            else
+                return View();
+
+        }
+    }
+}
